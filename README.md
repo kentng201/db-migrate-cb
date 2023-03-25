f04e9ed34282166b8959959aad09983819e9f7e2
Kent Ng <kent.ng201@gmail.com>
2023-03-25 15:54:18 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,106 @@
+# db-migrate-cb
+
+This is a driver for couchbase data migrations. It is based on the [db-migrate](https://db-migrate.readthedocs.io/en/latest/)
+
+## Getting Started
+
+```bash
+npm install db-migrate
+npm install db-migrate-cb
+```
+
+## Configuration
+
+To use this driver, create a `database.json` file with information similar to the following:
+
+```json
+{
+    "defaultEnv": "dev",
+    "dev": {
+        "driver": "cb",
+        "host": "couchbase://localhost",
+        "username": "username",
+        "password": "password",
+        "bucket": "default"
+    }
+}
+```
+
+## Create a migration
+
+You can create a migration with the following command:
+
+```bash
+db-migrate create <file_name>
+```
+
+## API
+
+### Create Collection
+
+You may call `createCollection` to create a collection in the database.
+
+```js
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = async function (db) {
+  await db.createCollection('test');
+};
+
+exports.down = async function (db) {
+  await db.dropCollection('test');
+};
+
+exports._meta = {
+  version: 1,
+};
+```
+
+Pass in the second parameter to `createCollection` to specify the scope. By default, the scope is `_default`.
+
+```js
+exports.up = async function (db) {
+  await db.createCollection('test', 'my_scope');
+};
+```
+
+### Drop Collection
+
+You may call `dropCollection` to create a collection in the database.
+Pass in the second parameter to `dropCollection` to specify the scope. By default, the scope is `_default`.
+
+```js
+exports.down = async function (db) {
+  await db.dropCollection('test', 'my_scope');
+};
+```
+
+### Create Index
+
+You may call `createIndex` to create an index in the database.
+
+```js
+exports.up = async function (db) {
+  await db.createIndex('test', 'test_index', ['test']);
+};
+```
+
+### Drop Index
+
+You may call `dropIndex` to drop an index in the database.
+
+```js
+exports.down = async function (db) {
+  await db.dropIndex('test', 'test_index');
+};
+```
---
+++ index.ts
@@ -0,0 +1,198 @@
+import {
+    CallbackFunction,
+    InternalOptions
+} from 'db-migrate-base';
+const Base = require('db-migrate-base');
+import Promise from 'bluebird';
+import {
+    Bucket,
+    Cluster,
+    ICollectionSpec
+} from 'couchbase';
+import moment from 'moment';
+
+let internals: InternalOptions = {
+    mod: {
+        log: 'info',
+        type: 'cb'
+    }
+};
+let config: CouchbaseConfig = {};
+
+export type CouchbaseConfig = {
+    host?: string;
+    username?: string;
+    password?: string;
+    bucket?: string;
+    scope?: string;
+    collection?: string;
+};
+
+let connection: Cluster;
+let bucket: Bucket;
+let scopeName: string;
+let collectionName: string;
+
+const CouchbaseDriver = Base.extend({
+    init: function (conn: Cluster) {
+        this._super(internals);
+        connection = conn;
+
+        if (!config.bucket) {
+            throw new Error('Configuration must specify bucket');
+        }
+        try {
+            bucket = connection.bucket(config.bucket);
+        } catch (err) {
+            if (err) {
+                throw new Error(`Could not open migration bucket: ${err}`);
+            }
+        }
+    },
+
+    close: function (callback?: CallbackFunction) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                await connection.close();
+                resolve(true);
+            } catch (err) {
+                reject(err);
+            }
+        }).nodeify(callback);
+
+    },
+
+    startMigration: function (callback: CallbackFunction) {
+        return Promise.resolve(true).nodeify(callback);
+    },
+
+    endMigration: function (callback: CallbackFunction) {
+        return Promise.resolve(true).nodeify(callback);
+    },
+
+    allLoadedMigrations: function (callback?: CallbackFunction) {
+        return new Promise((resolve) => {
+            bucket.scope(scopeName).query(`
+                SELECT meta().id, name, run_on
+                FROM \`${bucket.name}\`.\`${scopeName}\`.\`${collectionName}\`
+                ORDER BY run_on DESC, name DESC
+            `).then((res) => res.rows).then(resolve);
+        }).nodeify(callback);
+    },
+
+    createMigrationsTable: function (callback?: CallbackFunction) {
+        return new Promise(async (resolve) => {
+            const spec: ICollectionSpec = {
+                name: collectionName,
+                scopeName
+            };
+            let collectionExisted;
+            try {
+                const collection = bucket.scope(scopeName).collection('migrations');
+                await collection.upsert('TestMigrationExist', { ok: true });
+                await collection.remove('TestMigrationExist');
+                collectionExisted = true;
+            } catch (err) {
+                collectionExisted = false;
+            }
+            if (!collectionExisted) {
+                console.log('[Info] Initialize migrations collection');
+                await bucket.collections().createCollection(spec, {});
+                await new Promise((res) => setTimeout(res, 1000));
+                const createIndexQuery = `CREATE PRIMARY INDEX \`#primary\` ON \`${bucket.name}\`.\`${scopeName}\`.\`${collectionName}\``;
+                await connection.query(createIndexQuery);
+                console.log('[Info] Initialized migrations collection');
+            }
+            resolve();
+        }).nodeify(callback);
+    },
+
+    addMigrationRecord: function (migrationName: string, callback?: CallbackFunction) {
+        migrationName = migrationName.replaceAll('/', '');
+        return new Promise((resolve) => {
+            bucket
+                .scope(scopeName)
+                .collection(collectionName)
+                .upsert(migrationName, {
+                    name: migrationName,
+                    run_on: moment().toISOString()
+                })
+                .then(resolve);
+        }).nodeify(callback);
+    },
+
+    deleteMigration: function (migrationName: string, callback?: CallbackFunction) {
+        migrationName = migrationName.replaceAll('/', '');
+        return new Promise(async (resolve) => {
+            try {
+                const isMigrated = await bucket.scope(scopeName).collection(collectionName).get(migrationName);
+                if (isMigrated) {
+                    await bucket.scope(scopeName).collection(collectionName).remove(migrationName);
+                }
+                resolve();
+            } catch (error) {
+                resolve();
+            }
+        }).nodeify(callback);
+    },
+
+    createCollection: function (collectionName: string, scope?: string, callback?: CallbackFunction) {
+        scope = scope || scopeName;
+        return new Promise(async (resolve) => {
+            const spec: ICollectionSpec = {
+                name: collectionName,
+                scopeName
+            };
+            await bucket.collections().createCollection(spec, {});
+            await new Promise((res) => setTimeout(res, 1000));
+            const createIndexQuery = `CREATE PRIMARY INDEX \`#primary\` ON \`${bucket.name}\`.\`${scope}\`.\`${collectionName}\``;
+            await connection.query(createIndexQuery);
+            resolve();
+        }).nodeify(callback);
+    },
+
+    dropCollection: function (collectionName: string, scope?: string, callback?: CallbackFunction) {
+        scope = scope || scopeName;
+        return new Promise(async (resolve) => {
+            await bucket.collections().dropCollection(collectionName, scope as string);
+            await new Promise((res) => setTimeout(res, 1000));
+            resolve();
+        }).nodeify(callback);
+    },
+
+    addIndex(collectionName: string, indexName: string, columns: string[], scope?: string, callback?: CallbackFunction) {
+        scope = scope || scopeName;
+        return new Promise(async (resolve) => {
+            const indexQuery = `CREATE INDEX \`${indexName}\` ON \`${bucket.name}\`.\`${scope}\`.\`${collectionName}\`(${columns.join(', ')})`;
+            await connection.query(indexQuery);
+            resolve();
+        }).nodeify(callback);
+    },
+
+    removeIndex(collectionName: string, indexName: string, scope?: string, callback?: CallbackFunction) {
+        scope = scope || scopeName;
+        return new Promise(async (resolve) => {
+            const indexQuery = `DROP INDEX \`${bucket.name}\`.\`${scope}\`.\`${collectionName}\`.\`${indexName}\``;
+            await connection.query(indexQuery);
+            resolve();
+        }).nodeify(callback);
+    }
+});
+
+Promise.promisifyAll(CouchbaseDriver);
+
+exports.connect = async function (conf: CouchbaseConfig, intern?: InternalOptions, callback?: CallbackFunction) {
+    config = conf;
+    scopeName = config.scope || '_default';
+    collectionName = config.collection || 'migrations';
+
+    const host = config.host || process.env.HOST as string;
+    const db = await Cluster.connect(host, { username: config.username, password: config.password });
+    if (intern) {
+        internals = intern;
+    }
+
+    if (callback) {
+        callback(null, new CouchbaseDriver(db));
+    }
+};
